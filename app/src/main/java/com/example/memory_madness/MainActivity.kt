e2ce943db474b407b3bad68c6069f247c20e2371
PatNoO <Noordh94@hotmail.se>
2025-11-18 18:18:36 +0100

    initial commit

---
+++ app/src/main/java/com/example/memory_madness/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.memory_madness
+
+import android.os.Bundle
+
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_main)
+    }
+}
