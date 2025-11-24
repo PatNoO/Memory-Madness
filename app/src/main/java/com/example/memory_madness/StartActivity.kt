e66c01f92d14b69b2f0737723f3246b6a72d0589
PatNoO <Noordh94@hotmail.se>
2025-11-24 22:38:48 +0100

    Added binding in start activity

--- app/src/main/java/com/example/memory_madness/StartActivity.kt
+++ app/src/main/java/com/example/memory_madness/StartActivity.kt
@@ -1,15 +1,27 @@
 package com.example.memory_madness
 
+import android.os.Build
 import android.os.Bundle
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
+import com.example.memory_madness.databinding.ActivityStartBinding
 
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityStartBinding
+    private var player : Player? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        setContentView(R.layout.activity_start)
-
+        player = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU){
+            intent.getSerializableExtra("player", Player::class.java)
+        } else {
+            intent.getSerializableExtra("player") as Player
+        }
 
 
     }
