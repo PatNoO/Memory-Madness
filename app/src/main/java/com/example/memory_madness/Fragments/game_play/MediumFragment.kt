f1a7dc8c22b95688fdc5efaa53be54e16a49ac7a
PatNoO <Noordh94@hotmail.se>
2025-11-28 12:55:37 +0100

    Started adding code in medium fragment and fixed layout dimensoinratio

--- app/src/main/java/com/example/memory_madness/Fragments/game_play/MediumFragment.kt
+++ app/src/main/java/com/example/memory_madness/Fragments/game_play/MediumFragment.kt
@@ -5,22 +5,60 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.memory_madness.GameViewModel
+import com.example.memory_madness.PlayerViewModel
 import com.example.memory_madness.R
+import com.example.memory_madness.databinding.FragmentMediumBinding
 
 
 class MediumFragment : Fragment() {
 
+    private lateinit var binding : FragmentMediumBinding
+    private lateinit var gameViewModel: GameViewModel
+    private lateinit var playerViewModel : PlayerViewModel
+
+//    private lateinit var cardId = MutableList(R.drawable.)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        playerViewModel = ViewModelProvider(requireActivity())[PlayerViewModel::class.java]
+        gameViewModel = ViewModelProvider(requireActivity())[GameViewModel::class.java]
 
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medium, container, false)
+    ): View {
+        binding = FragmentMediumBinding.inflate(inflater,container,false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val containerId = listOf(
+            binding.card1Fm,
+            binding.card2Fm,
+            binding.card3Fm,
+            binding.card4Fm,
+            binding.card5Fm,
+            binding.card6Fm,
+            binding.card7Fm,
+            binding.card8Fm,
+            binding.card9Fm,
+            binding.card10Fm,
+            binding.card11Fm,
+            binding.card12Fm,
+            binding.card13Fm,
+            binding.card14Fm,
+            binding.card15Fm,
+            binding.card16Fm,
+            binding.card17Fm,
+            binding.card18Fm
+        )
     }
 
 }
