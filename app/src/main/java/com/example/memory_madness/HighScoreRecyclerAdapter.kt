d9c1dd4bbcd96d5bfd597da53b194f1d6ff1c93a
PatNoO <Noordh94@hotmail.se>
2025-12-06 22:53:13 +0100

    Added a RecyclerAdapter and implemented needed code to update player High Score Updated code in HighScoreFragment for RecyclerView instead of listView

---
+++ app/src/main/java/com/example/memory_madness/HighScoreRecyclerAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.memory_madness
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.memory_madness.DataClass.Player
+import com.example.memory_madness.databinding.FragmentRecyclerBinding
+import com.example.memory_madness.databinding.ListHighScoreItemBinding
+
+class HighScoreRecyclerAdapter (private var players: List<Player> ) : RecyclerView.Adapter<HighScoreRecyclerAdapter.HighScoreViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): HighScoreViewHolder {
+        val binding = ListHighScoreItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return HighScoreViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: HighScoreViewHolder,
+        position: Int
+    ) {
+        val player = players[position]
+
+        val minutes = player.time?.div(60)
+        val seconds = player.time?.rem(60)
+
+                holder.binding.tvPlayerName.text = "Name : ${player.name}"
+                holder.binding.tvDifficulty.text ="Difficulty : ${player.difficulty}"
+                holder.binding.tvPause.text = "Pause help : ${player.pauseChoice}"
+                holder.binding.tvTimeLeft.text = "Time Left : $minutes : $seconds"
+                holder.binding.tvMoves.text = "Moves : ${player.moves}"
+    }
+
+    override fun getItemCount(): Int {
+        return players.size
+    }
+
+    fun updateList (updatedList : List<Player>) {
+        players = updatedList
+        notifyDataSetChanged()
+    }
+
+
+    inner class HighScoreViewHolder(val binding : ListHighScoreItemBinding) : RecyclerView.ViewHolder(binding.root)
+}
