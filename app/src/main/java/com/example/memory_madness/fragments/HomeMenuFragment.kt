46eac7fc520bed7683511221e4e478a967a3d4dc
PatNoO <Noordh94@hotmail.se>
2025-12-07 21:21:47 +0100

    Added if () to check if etChangedNameText is empty or null so that player can't add a empty name

--- app/src/main/java/com/example/memory_madness/fragments/HomeMenuFragment.kt
+++ app/src/main/java/com/example/memory_madness/fragments/HomeMenuFragment.kt
@@ -2,6 +2,7 @@ package com.example.memory_madness.fragments
 
 import android.app.AlertDialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -85,9 +86,14 @@ class HomeMenuFragment : Fragment() {
      */
     private fun changeNameButton() {
         binding.btnEnterNameFhm.setOnClickListener {
-            playerViewModel.changeName(binding.etChangeNameFhm.text.toString())
-
+            if (binding.etChangeNameFhm.text.isNullOrEmpty()){
+                return@setOnClickListener
+            }else {
+                playerViewModel.changeName(binding.etChangeNameFhm.text.toString())
+                Log.i("!!!", "${playerViewModel.player.value}")
+            }
         }
+
     }
 
     /**
